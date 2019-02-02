d7048eb9a4a38fec38faf39e78a7bf7eac4ecfa7
julien <wawrzyniakjulien@gmail.com>
2019-02-02 15:33:51 +0100

    :sparkles: Add level1

--- level1/main.rb
+++ level1/main.rb
@@ -1,3 +1,43 @@
 require "json"
 
-# your code
+class NightShiftManager
+  def initialize(filePath)
+    file = File.read filePath
+
+    @data = JSON.parse(file)
+  end
+
+  def getWorkerShifts(id)
+    return @data['shifts'].find_all { |shift| shift['user_id'] == id }
+  end
+
+  def writeJSON(data, filePath)
+    File.open(filePath,'w') do |f|
+      f.write(JSON.pretty_generate(data))
+    end
+  end
+
+  def workersPrices(outputFile)
+    output = {
+      workers: []
+    }
+
+    @data['workers'].each do |worker|
+      workerShifts = getWorkerShifts(worker['id'])
+      workerPrice = worker['price_per_shift'] * workerShifts.length
+
+      output[:workers].push({ id: worker['id'], price: workerPrice })
+    end
+
+    writeJSON(output, outputFile)
+
+    puts 'Done.'
+  end
+
+  private :getWorkerShifts, :writeJSON
+end
+
+ns = NightShiftManager.new('./data.json')
+
+ns.workersPrices('output.json')
+
