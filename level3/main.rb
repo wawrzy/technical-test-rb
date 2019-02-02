46a0eb9cd8a14f196d4b2e2a8bf895344dc53f40
julien <wawrzyniakjulien@gmail.com>
2019-02-02 17:21:14 +0100

    :sparkles: Add level 3

--- level3/main.rb
+++ level3/main.rb
@@ -1,4 +1,5 @@
 require "json"
+require "date"
 
 WORKERS_STATUS = {
   medic: {
@@ -8,6 +9,9 @@ WORKERS_STATUS = {
     price_per_shift: 126
   }
 }
+
+SHIFT_RATE = 1
+WEEKEND_SHIFT_RATE = SHIFT_RATE * 2
 
 class NightShiftManager
   def initialize(filePath)
@@ -26,6 +30,17 @@ class NightShiftManager
     return WORKERS_STATUS[worker['status'].to_sym][:price_per_shift]
   end
 
+  def getShiftsRate(shifts)
+    rate = 0
+
+    shifts.each do |shift|
+      day = Date.parse(shift['start_date'])
+       rate += day.sunday? || day.saturday? ? WEEKEND_SHIFT_RATE : SHIFT_RATE
+    end
+
+    return rate
+  end
+
   def writeJSON(data, filePath)
     File.open(filePath,'w') do |f|
       f.write(JSON.pretty_generate(data))
@@ -41,7 +56,8 @@ class NightShiftManager
       workerId = worker['id']
 
       workerShifts = getWorkerShifts(workerId)
-      workerPrice = getWorkerPrice(workerId) * workerShifts.length
+      shiftsRate = getShiftsRate(workerShifts)
+      workerPrice = getWorkerPrice(workerId) * shiftsRate
 
       output[:workers].push({ id: workerId, price: workerPrice })
     end
@@ -51,7 +67,7 @@ class NightShiftManager
     puts 'Done.'
   end
 
-  private :getWorkerShifts, :getWorkerPrice, :writeJSON
+  private :getWorkerShifts, :getWorkerPrice, :getShiftsRate, :writeJSON
 end
 
 ns = NightShiftManager.new('./data.json')
